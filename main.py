b0a4c6db1f26a53e7d565233a694d2d91da43b32
Omar Shehata <omar.sameh.shehata@gmail.com>
2016-12-02 11:03:30 -0600

    Added convenient rotor controls

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import rotor_control as rc
+import time
 
 rc.test_rotors()
+
+rc.set_rotors()
+time.sleep(2)
+rc.set_rotors(a=-1,b=-1,c=-1,d=-1)
--- rotor_control.py
+++ rotor_control.py
@@ -9,9 +9,22 @@ import navio.util
 SERVO_MIN = 1.18 #1.250 #ms
 SERVO_MAX = 1.750 #ms
 
+pwmArray = []
 
 def calibrate():
     # TODO: Find max and min and save them
+    return 0
+
+def set_rotors(**kwargs):
+    # Takes values from 0 to 1 where 0 is min spinning, 1 is max spinning, and -1 is stop
+    def convert(val):
+        if(val == -1): return 0
+        return val * (SERVO_MAX - SERVO_MIN) + SERVO_MIN
+
+    if('a' in kwargs): pwmArray[2].set_duty_cycle(convert(kwargs['a']))
+    if('b' in kwargs): pwmArray[0].set_duty_cycle(convert(kwargs['b']))
+    if('c' in kwargs): pwmArray[3].set_duty_cycle(convert(kwargs['c']))
+    if('d' in kwargs): pwmArray[1].set_duty_cycle(convert(kwargs['d']))
 
 def test_rotors():
     navio.util.check_apm()
@@ -22,7 +35,7 @@ def test_rotors():
 
     # Now every time we want to use a motor, we have to enable it first
     # Let's enable all 4 (they're numbered 0-3)
-    pwmArray = []
+
     for num in range(0,3+1): # This loops over 0,1,2,3
     	pwm = navio.pwm.PWM(num)
     	pwm.set_period(50)
@@ -37,10 +50,10 @@ def test_rotors():
         for pwm in pwmArray:
             pwm.set_duty_cycle(SERVO_MIN)
             # They will keep spinning until we send a new signal
-            time.sleep(1)
+            time.sleep(.1)
             for pwm in pwmArray:
         		pwm.set_duty_cycle(SERVO_MIN)
-            time.sleep(1)
+            time.sleep(.1)
 
     # So even if you terminate the script, they will keep spinning
     # We have to send a stop signal to every motor before we leave
